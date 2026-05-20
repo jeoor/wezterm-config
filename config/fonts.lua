21ee03011b72a230aed7733e472d3e8548e7ad3d
Kayro <i@kayro.cn>
2026-05-20 23:57:52 +0800

    style: 中文采用鸿蒙字体

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,10 +2,14 @@ local wezterm = require("wezterm")
 local platform = require("utils.platform")
 
 local font = "JetBrainsMono NF"
+local cjk_font = "HarmonyOS Sans SC"
 local font_size = platform.is_mac and 14 or (platform.is_win and 12 or 11)
 
 return {
-  font = wezterm.font({ family = font, weight = "Medium" }),
+  font = wezterm.font_with_fallback({
+    { family = font, weight = "Medium" },
+    { family = cjk_font, weight = "Medium" },
+  }),
   font_size = font_size,
 
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
